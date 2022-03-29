c8c0c1768c7746f460c28e0dfd86e340524aee70
arturogtzarturo2 <99524370+arturogtzarturo2@users.noreply.github.com>
2022-03-28 21:08:09 -0700

    descripcion

---
+++ AwesomeProject/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Caca</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
