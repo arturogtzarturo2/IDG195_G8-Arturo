3530c593750dfaf111b9433834c2330920c70df1
arturogtzarturo2 <99524370+arturogtzarturo2@users.noreply.github.com>
2022-03-28 21:13:48 -0700

    descripcion

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Caca</Text>
+      <Text>Hola profe Andrea como sta</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0000',
+    backgroundColor: '#CCADFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
